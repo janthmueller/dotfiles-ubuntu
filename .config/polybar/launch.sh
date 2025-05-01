f6632dfe6c8eba87d42b9773e64a21f3cc929afb
janthmueller <mail@jantmueller.com>
2025-05-01 12:24:51 +0200

    polybar; vimotions in i3

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Kill any existing polybar
 killall -q polybar
 
-# Wait until the processes are gone
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.2; done
 
-# Launch main bar (you can name it "example" or "top" depending on your config)
-polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+polybar main &
 
