68373bc1b659c7234a7ca45f71d30c50ceca99e7
janthmueller <mail@jantmueller.com>
2025-06-04 10:24:42 +0200

    format on w

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -19,6 +19,14 @@ return {
       automatic_installation = true,
     })
 
+    -- Format on save
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      callback = function()
+        vim.lsp.buf.format({ async = false })
+      end,
+    })
+
+    -- Optional: Manual format keybinding
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
   end,
 }
