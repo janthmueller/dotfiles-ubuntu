34f62859c1353bbb50352c76f38a5fa747c02a9e
janthmueller <mail@jantmueller.com>
2025-06-04 22:02:47 +0200

    lsp rename, yank2clip

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,33 +1,33 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "jay-babu/mason-null-ls.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"jay-babu/mason-null-ls.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
-      },
-    })
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
+			},
+		})
 
-    require("mason-null-ls").setup({
-      ensure_installed = { "stylua", "black", "isort" },
-      automatic_installation = true,
-    })
+		require("mason-null-ls").setup({
+			ensure_installed = { "stylua", "black", "isort" },
+			automatic_installation = true,
+		})
 
-    -- Format on save
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      callback = function()
-        vim.lsp.buf.format({ async = false })
-      end,
-    })
+		-- Format on save
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			callback = function()
+				vim.lsp.buf.format({ async = false })
+			end,
+		})
 
-    -- Optional: Manual format keybinding
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+		-- Optional: Manual format keybinding
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP Rename Symbol" })
+	end,
 }
-
